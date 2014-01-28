c84f76c0c960edb791765b8d73b98386e44f4c3e
Timo Koola <timo.kooola@gmail.com>
2014-01-28 02:29:44 +0200

    many things there

--- streaming.py
+++ streaming.py
@@ -4,7 +4,7 @@ import random, re, daemon, lockfile
 from pymongo import MongoClient
 import json
 
-answerre = re.compile('(\d+)[dD](\d+)([+-]?\d*)')
+answerre = re.compile('([a-cA-C]+)')
 useraccount = "@gallupbot"
 db_collection = None
 
@@ -13,11 +13,10 @@ def handle_command_line():
     parser.add_argument("-t", "--test", help="Get a test response")
     parser.add_argument("-k", "--keyfile", help="Twitter account consumer and accesstokens")
     parser.add_argument("-u", "--username", help="Username to follow", default="@gallupbot")
+    parser.add_argument("-q", "--question", help="Question to tweet")
+    parser.add_argument("-s", "--summary", help="Get stats for the last tweeted question")
     args = parser.parse_args()
     return args
-
-
-
 
 
 class AnswerParser:
@@ -26,21 +25,18 @@ class AnswerParser:
         if not mth:
              mth = answerre.match(prev)
         if mth:
-            self.die = Dice(mth.groups())
+            self.answer = mth.groups()[0]
         else:
-            self.die = Dice([1,6,0])
+            self.answer = "EMT"
 
 class ReplyGenerator:
-    def __init__(self,tweet,prev,counter):
+    def __init__(self,tweet):
         self.tweet = tweet
-        self.prev = prev
-        dp = AnswerParser(tweet.split()[1],prev)
-        self.dice = dp.die
-        self.dice.counter = counter
-        self.dice.roll()
+        dp = AnswerParser(tweet.split()[1],"EMT")
+        self.answer = dp.answer
 
     def reply(self):
-        reply = "%s" % (self.dice.lastRollStr)
+        reply = "%s" % (self.answer)
         return reply
 
 
@@ -60,25 +56,23 @@ class CustomStreamListener(tweepy.StreamListener):
                                       status.created_at, 
                                       status.source,)
             #yeah, needs refactoring
-            s = {}
-            s["text"] = status.text
-            s["name"] = status.author.screen_name
-            s["timestamp"] = status.created_at
-            s["source"] = status.source
-            items = db_collection.find({"counter":{"$exists": "true"}})
-            l = [5]
-            try:
-                while True:
-                    item = items.next()
-                    l.append(item["counter"])
-            except StopIteration:
-                counter = max(l)
-            s["counter"] = counter + 1
-            if status.text.find(useraccount) == 0:
-                rg = ReplyGenerator(status.text,item["last_roll"], counter)
-                s["last_roll"] = repr(rg.dice)
-                self.tweepyapi.update_status(rg.reply())
-            db_collection.insert(s)
+            # first  find the latest status we have created
+            #self.tweepyapi.
+            latest_id = latest_question()["id"]
+            name = status.author.screen_name
+
+            prev_answer = db_collection.find({"type": "answer", "name": name})
+
+            if prev_answer.count() > 0:
+                s = prev_answer[0]
+            else:
+                s = {}
+                s["text"] = status.text
+                s["name"] = name
+                s["timestamp"] = status.created_at
+            ap = AnswerParser(status.text)
+            s["answer"] = ap.answer
+            db_collection.save(s)
 
         except Exception, e:
             print >> sys.stderr, 'Encountered Exception:', e
@@ -106,17 +100,48 @@ class TweepyHelper:
         auth.set_access_token(accesstoken, accesssec)
         listener = CustomStreamListener()
         listener.tweepyapi = tweepy.API(auth)
+        self.tweepyapi = listener.tweepyapi
         self.api = tweepy.streaming.Stream(auth, listener, timeout=60)
+
+class NormalTweepyHelper:
+    def __init__(self,keyfile):
+        f = open(keyfile)
+        lines = f.readlines()
+        f.close()
+        consumerkey = lines[0].split("#")[0]
+        consumersecret = lines[1].split("#")[0]
+        accesstoken = lines[2].split("#")[0]
+        accesssec = lines[3].split("#")[0]
+
+        auth = tweepy.OAuthHandler(consumerkey, consumersecret)
+        auth.set_access_token(accesstoken, accesssec)
+        self.api = tweepy.API(auth)
 
 def run(api):
     api.filter(track=[useraccount])
 
+def store_question(tweet):
+    qs = {}
+    qs["tweetid"] = tweet.id
+    qs["text"] = tweet.text
+    qs["type"] = "question"
+    qs["timestamp"] = tweet.created_at
+    db_collection.insert(qs) 
 
+def latest_question():
+    return db_collection.find({"type":"question"}).sort("_id", -1).limit(1)[0]
+
+def question_from_file(f):
+    fr = open(f)
+    text = fr.read()
+    fr.close
+    return text
+    
 def connect_to_db():
     global db_collection
     client = MongoClient()
-    db = client.dicestream
-    db_collection = db.dicecollection
+    db = client.answerstream
+    db_collection = db.answercollection
 
 
 if __name__ == "__main__":
@@ -124,6 +149,15 @@ if __name__ == "__main__":
     connect_to_db()
     useraccount = args.username
     api = (TweepyHelper(args.keyfile)).api
+    if args.question:
+        twapi = (NormalTweepyHelper(args.keyfile)).api
+        tweet = twapi.update_status(question_from_file(args.question))
+        store_question(tweet)
+        sys.exit(0)
+    if args.summary:
+        print latest_question()
+        sys.exit(0)
+
     ferr = open("runningstreamingerrs.txt", "w+")
     fout = open("runningstreamingout.txt", "w+")
     if not args.test:
